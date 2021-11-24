0ea95297e7098c997829ea1ba83a170f1a568e6f
FRanggaY <franciscusrangga@gmail.com>
2021-11-25 03:19:16 +0700

    change to statefull

--- src/components/section/home/index.js
+++ src/components/section/home/index.js
@@ -1,5 +1,4 @@
-import React from 'react';
-// import './section.css';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 import Gap from '../../atom/gap';
@@ -37,59 +36,61 @@ const Comics = styled.div`
 
 `;
 
-function HomeSection() {
-    return (
-        <Middle>
-            <Gap width={10} height={20} />
-            <Populars>
-                <CardPopular 
-                    title={'Jujutsu Kaisen'}
-                    img={'https://cdn.myanimelist.net/images/manga/3/210341l.jpg?_gl=1*gsnxap*_ga*MjE0Mjk4MTMzNS4xNjM2OTQxNzY0*_ga_26FEP9527K*MTYzNjk1MDkyOC4yLjEuMTYzNjk1MDkyOS41OQ..'}
-                />
-                <CardPopular 
-                    title={'One Punch Man'}
-                    img={'https://cdn.myanimelist.net/images/manga/3/80661l.jpg?_gl=1*1dl22io*_ga*MjE0Mjk4MTMzNS4xNjM2OTQxNzY0*_ga_26FEP9527K*MTYzNjk1NjU3NS4zLjEuMTYzNjk1Njc3Ny4xNQ..'}
-                />
-                <CardPopular 
-                    title={'Overlord'}
-                    img={'https://cdn.myanimelist.net/images/manga/3/161407l.jpg'}
-                />
-                <CardPopular 
-                    title={'Berserk Of Gluttony'}
-                    img={'https://cdn.myanimelist.net/images/manga/3/212091l.jpg?_gl=1*1mdd0i0*_ga*MjE0Mjk4MTMzNS4xNjM2OTQxNzY0*_ga_26FEP9527K*MTYzNjk1NjU3NS4zLjEuMTYzNjk1NjczNi41Ng..'}
-                />
-            </Populars>
-
-            <Comics>
-                <CardComic 
-                    title={'Kage no Jitsuryokusha ni Naritakute!'} 
-                    category={'Manga'} 
-                    img={'https://cdn.myanimelist.net/images/manga/3/227280l.jpg'} 
-                    score={'8.96'}/>
-                <CardComic 
-                    title={'Solo Levelling'} 
-                    category={'Manhwa'} 
-                    img={'https://cdn.myanimelist.net/images/manga/3/222295l.jpg?_gl=1*1khwg9d*_ga*MjE0Mjk4MTMzNS4xNjM2OTQxNzY0*_ga_26FEP9527K*MTYzNjk1NjU3NS4zLjEuMTYzNjk1NjY0NS41NA..'} 
-                    score={'8.00'}/>
-                <CardComic 
-                    title={'Mashle: Magic and Muscles'} 
-                    category={'Manga'} 
-                    img={'https://cdn.myanimelist.net/images/manga/1/230705l.jpg'} 
-                    score={'8.00'}/>
-                <CardComic 
-                    title={'Noragami'} 
-                    category={'Manga'} 
-                    img={'https://cdn.myanimelist.net/images/manga/2/90899l.jpg?_gl=1*6mugao*_ga*NTE3MzgzNjIuMTYzNzYzNDk5OQ..*_ga_26FEP9527K*MTYzNzYzNTAwMC4xLjEuMTYzNzYzNTA1Ni40'} 
-                    score={'8.96'}/>
-                <CardComic 
-                    title={'Enen no Shouboutai'} 
-                    category={'Manga'} 
-                    img={'https://cdn.myanimelist.net/images/manga/3/178996l.jpg?_gl=1*1xyfdes*_ga*NTE3MzgzNjIuMTYzNzYzNDk5OQ..*_ga_26FEP9527K*MTYzNzYzNTAwMC4xLjEuMTYzNzYzNTEyMi41'} 
-                    score={'8.96'}/>
-            </Comics>
-
-        </Middle>
-    )
+class HomeSection extends Component {
+    render() {
+        return (
+            <Middle>
+                <Gap width={10} height={20} />
+                <Populars>
+                    <CardPopular 
+                        title={'Jujutsu Kaisen'}
+                        img={'https://cdn.myanimelist.net/images/manga/3/210341l.jpg?_gl=1*gsnxap*_ga*MjE0Mjk4MTMzNS4xNjM2OTQxNzY0*_ga_26FEP9527K*MTYzNjk1MDkyOC4yLjEuMTYzNjk1MDkyOS41OQ..'}
+                    />
+                    <CardPopular 
+                        title={'One Punch Man'}
+                        img={'https://cdn.myanimelist.net/images/manga/3/80661l.jpg?_gl=1*1dl22io*_ga*MjE0Mjk4MTMzNS4xNjM2OTQxNzY0*_ga_26FEP9527K*MTYzNjk1NjU3NS4zLjEuMTYzNjk1Njc3Ny4xNQ..'}
+                    />
+                    <CardPopular 
+                        title={'Overlord'}
+                        img={'https://cdn.myanimelist.net/images/manga/3/161407l.jpg'}
+                    />
+                    <CardPopular 
+                        title={'Berserk Of Gluttony'}
+                        img={'https://cdn.myanimelist.net/images/manga/3/212091l.jpg?_gl=1*1mdd0i0*_ga*MjE0Mjk4MTMzNS4xNjM2OTQxNzY0*_ga_26FEP9527K*MTYzNjk1NjU3NS4zLjEuMTYzNjk1NjczNi41Ng..'}
+                    />
+                </Populars>
+    
+                <Comics>
+                    <CardComic 
+                        title={'Kage no Jitsuryokusha ni Naritakute!'} 
+                        category={'Manga'} 
+                        img={'https://cdn.myanimelist.net/images/manga/3/227280l.jpg'} 
+                        score={'8.96'}/>
+                    <CardComic 
+                        title={'Solo Levelling'} 
+                        category={'Manhwa'} 
+                        img={'https://cdn.myanimelist.net/images/manga/3/222295l.jpg?_gl=1*1khwg9d*_ga*MjE0Mjk4MTMzNS4xNjM2OTQxNzY0*_ga_26FEP9527K*MTYzNjk1NjU3NS4zLjEuMTYzNjk1NjY0NS41NA..'} 
+                        score={'8.00'}/>
+                    <CardComic 
+                        title={'Mashle: Magic and Muscles'} 
+                        category={'Manga'} 
+                        img={'https://cdn.myanimelist.net/images/manga/1/230705l.jpg'} 
+                        score={'8.00'}/>
+                    <CardComic 
+                        title={'Noragami'} 
+                        category={'Manga'} 
+                        img={'https://cdn.myanimelist.net/images/manga/2/90899l.jpg?_gl=1*6mugao*_ga*NTE3MzgzNjIuMTYzNzYzNDk5OQ..*_ga_26FEP9527K*MTYzNzYzNTAwMC4xLjEuMTYzNzYzNTA1Ni40'} 
+                        score={'8.96'}/>
+                    <CardComic 
+                        title={'Enen no Shouboutai'} 
+                        category={'Manga'} 
+                        img={'https://cdn.myanimelist.net/images/manga/3/178996l.jpg?_gl=1*1xyfdes*_ga*NTE3MzgzNjIuMTYzNzYzNDk5OQ..*_ga_26FEP9527K*MTYzNzYzNTAwMC4xLjEuMTYzNzYzNTEyMi41'} 
+                        score={'8.96'}/>
+                </Comics>
+    
+            </Middle>
+        )
+    }
 }
 
 export default HomeSection;
